f7c60df184181c8fa1157a8580b6ecdaf5186bd7
Justus Kandzi <justus.kandzi@kitchenstories.de>
2015-12-29 21:16:13 +0100

    make progress bars configurable

--- Sources/Progress.swift
+++ Sources/Progress.swift
@@ -27,70 +27,70 @@
 
 import Foundation
 
-public struct ProgressGenerator<G: GeneratorType>: GeneratorType {
-    var source: G
-    var index = 0
-    let startTime = CFAbsoluteTimeGetCurrent()
-    let barLength = 30
-    let count: Int
-    
-    init(source: G, count: Int) {
-        self.source = source
-        self.count = count
+// MARK: - ProgressElements
+
+
+public protocol ProgressElementType {
+    func value(progressBar: ProgressBar) -> String
+}
+
+
+/// creates the progress bar element e.g. "[----------------------        ]"
+public struct ProgressBarLine: ProgressElementType {
+    let barLength: Int
+
+    init(barLength: Int = 30) {
+        self.barLength = barLength
     }
     
-    public mutating func next() -> G.Element? {
-        let string = [indexInformation(), progressBar(), timeEstimates()].joinWithSeparator(" ")
-        
-        if index == 0 {
-            // "\u{1B}[1A" moves the cursor up one line. have to add an empty line here.
-            print("")
-        }
-        
-        print("\u{1B}[1A\u{1B}[K\(string)")
-        
-        index += 1
-        return source.next()
-    }
-    
-    /// creates the index element e.g. "2 of 3"
-    private func indexInformation() -> String {
-        return "\(index) of \(count)"
-    }
-    
-    /// creates the progress bar element e.g. "[----------------------        ]"
-    private func progressBar() -> String {
+    public func value(progressBar: ProgressBar) -> String {
         var completedBarElements = 0
-        if count == 0 {
+        if progressBar.count == 0 {
             completedBarElements = barLength
-        }
-        else {
-            completedBarElements = Int(Double(barLength) * (Double(index) / Double(count)))
+        } else {
+            completedBarElements = Int(Double(barLength) * (Double(progressBar.index) / Double(progressBar.count)))
         }
         
         var barArray = [String](count: completedBarElements, repeatedValue: "-")
         barArray += [String](count: barLength - completedBarElements, repeatedValue: " ")
         return "[" + barArray.joinWithSeparator("") + "]"
     }
-    
-    /// creates the time estimates e.g. "ETA: 00:00:02 (at 1.00 it/s)"
-    private func timeEstimates() -> String {
-        let totalTime = CFAbsoluteTimeGetCurrent() - startTime
+}
+
+
+/// creates the index element e.g. "2 of 3"
+public struct ProgressIndex: ProgressElementType {
+    public func value(progressBar: ProgressBar) -> String {
+        return "\(progressBar.index) of \(progressBar.count)"
+    }
+}
+
+
+public struct ProgressPercent: ProgressElementType {
+    public func value(progressBar: ProgressBar) -> String {
+        let percentDone = Double(progressBar.index) / Double(progressBar.count) * 100
+        return String(format: "%.1f%%", percentDone)
+    }
+}
+
+
+/// creates the time estimates e.g. "ETA: 00:00:02 (at 1.00 it/s)"
+public struct ProgressTimeEstimates: ProgressElementType {
+    public func value(progressBar: ProgressBar) -> String {
+        let totalTime = CFAbsoluteTimeGetCurrent() - progressBar.startTime
         
         var itemsPerSecond = 0.0
         var estimatedTimeRemaining = 0.0
-        if index > 0 {
-            itemsPerSecond = totalTime / Double(index)
-            estimatedTimeRemaining = itemsPerSecond * Double(count - index)
+        if progressBar.index > 0 {
+            itemsPerSecond = totalTime / Double(progressBar.index)
+            estimatedTimeRemaining = itemsPerSecond * Double(progressBar.count - progressBar.index)
         }
         
-        return "ETA: \(formatDuration(estimatedTimeRemaining)) (at \(formatDouble(itemsPerSecond, ".2")) it/s)"
+        let estimatedTimeRemainingString = formatDuration(estimatedTimeRemaining)
+        
+        return String(format: "ETA: %@ (at %.2f) it/s)", estimatedTimeRemainingString, itemsPerSecond)
     }
-    
-    private func formatDouble(value: Double, _ format: String) -> String {
-        return String(format: "%\(format)f", value)
-    }
-    
+
     private func formatDuration(duration: NSTimeInterval) -> String {
         let duration = Int(duration)
         let seconds = duration % 60
@@ -100,15 +100,67 @@ public struct ProgressGenerator<G: GeneratorType>: GeneratorType {
     }
 }
 
+// MARK: - ProgressBar
+
+
+public struct ProgressBar {
+    var index = 0
+    let startTime = CFAbsoluteTimeGetCurrent()
+    let count: Int
+    let configuration: [ProgressElementType]?
+
+    static var defaultConfiguration: [ProgressElementType] = [ProgressIndex(), ProgressBarLine(), ProgressTimeEstimates()]
+    
+    init(count: Int, configuration: [ProgressElementType]? = nil) {
+        self.count = count
+        self.configuration = configuration
+        
+        // the cursor is moved up before printing the progress bar.
+        // have to move the cursor down one line initially.
+        print("")
+    }
+    
+    mutating func next() {
+        let configuration = self.configuration ?? ProgressBar.defaultConfiguration
+        let string: String = configuration.reduce("", combine: { $0 + " " + $1.value(self) })
+        print("\u{1B}[1A\u{1B}[K\(string)")
+
+        index += 1
+    }
+}
+
+// MARK: - GeneratorType
+
+
+public struct ProgressGenerator<G: GeneratorType>: GeneratorType {
+    var source: G
+    var progressBar: ProgressBar
+    
+    init(source: G, count: Int, configuration: [ProgressElementType]? = nil) {
+        self.source = source
+        self.progressBar = ProgressBar(count: count, configuration: configuration)
+    }
+    
+    public mutating func next() -> G.Element? {
+        progressBar.next()
+        return source.next()
+    }
+}
+
+// MARK: - SequenceType
+
+
 public struct Progress<G: SequenceType>: SequenceType {
     let generator: G
+    let configuration: [ProgressElementType]?
     
-    public init(_ generator: G) {
+    public init(_ generator: G, configuration: [ProgressElementType]? = nil) {
         self.generator = generator
+        self.configuration = configuration
     }
     
     public func generate() -> ProgressGenerator<G.Generator> {
         let count = generator.underestimateCount()
-        return ProgressGenerator(source: generator.generate(), count: count)
+        return ProgressGenerator(source: generator.generate(), count: count, configuration: configuration)
     }
 }
