5aaeac0f486c6f5860e623a5537dfafb90def25a
Justus Kandzi <justus.kandzi@kitchenstories.de>
2015-12-29 11:40:07 +0100

    add carthage support

---
+++ Progress.swift/Progress.swift/Progress.swift.h
@@ -0,0 +1,19 @@
+//
+//  Progress.swift.h
+//  Progress.swift
+//
+//  Created by Justus Kandzi on 28/12/15.
+//  Copyright © 2015 Justus Kandzi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Progress.swift.
+FOUNDATION_EXPORT double Progress_swiftVersionNumber;
+
+//! Project version string for Progress.swift.
+FOUNDATION_EXPORT const unsigned char Progress_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Progress_swift/PublicHeader.h>
+
+
