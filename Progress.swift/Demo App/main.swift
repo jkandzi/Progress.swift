f399647d95f34ffc59dad16086210b068f65de24
Justus Kandzi <justus.kandzi@kitchenstories.de>
2015-12-29 17:35:37 +0100

    add demo app

---
+++ Progress.swift/Demo App/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Demo App
+//
+//  Created by Justus Kandzi on 29/12/15.
+//  Copyright © 2015 Justus Kandzi. All rights reserved.
+//
+
+import Foundation
+import Progress
+
+for i in Progress(0...9) {
+    sleep(1)
+}
+
+for i in Progress([12, 16, 337, 7, 99]) {
+    sleep(1)
+}
