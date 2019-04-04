73e968b418c83f5e04f6cf7d591bd84b10ff98bd
Brett Jones <bjones@lyft.com>
2019-04-04 09:52:27 -0700

    Update Package.swift to 4.0 syntax

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Progress"
+    name: "Progress",
+    products: [
+        .library(name: "Progress", targets: ["Progress"]),
+    ],
+    targets: [
+        .target(name: "Progress", dependencies: [], path: "Sources"),
+    ]
 )
